0249b8e23d83d018e8ae573710e716f1c678b915
wannabe20 <prchlik1819@gmail.com>
2024-12-10 11:00:31 +0100

    xxx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from 'flowbite/plugin';
+
 export default {
   content: [
     "./index.html",
@@ -9,7 +11,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
-  ]
-}
-
+    flowbite,
+  ],
+};
