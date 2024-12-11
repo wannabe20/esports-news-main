fbba2000c6eabf9615dea34c759df5de5904c2df
wannabe20 <prchlik1819@gmail.com>
2024-12-11 07:16:01 +0100

    fixfix

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { supabase } from "../supabase"; // Import Supabase client
+import { supabase } from "../../supabase";
 
 export const AuthContext = createContext();
 
--- supabase.js
+++ supabase.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+);
 
 export default supabase;
